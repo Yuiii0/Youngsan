68ecbd5b9fa835f12fc0e1cb98d29b8e1dc0056e
Yujiii0 <yuji918@naver.com>
2024-01-27 21:47:06 +0900

    feat

--- type.js
+++ type.js
@@ -1,12 +1,5 @@
-// new TypeIt("#type")
-//   .type(")
-//   .pause(1000)
-
-//   .type("용산을 소개합니다 🙌")
-//   .go();
-
 new TypeIt("#type", {
-  strings: "이제는 용산 시대 🙌",
+  strings: "용산 시대 🙌",
   speed: 75,
   loop: true,
 }).go();
